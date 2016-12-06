77852d621ad170f8a2fc3bfb995f0383457d3493
Karol <grooby-87@o2.pl>
2016-12-06 22:09:26 +0100

    Porządki przedświąteczne :]

--- Form1.cs
+++ Form1.cs
@@ -21,15 +21,11 @@ namespace tibicalc
 
         private void button1_Click(object sender, EventArgs e)
         {
-            object profesja = listBox1.SelectedItem;
-            //string profesja = listBox1.SelectedItem.ToString();
+            //object profesja = listBox1.SelectedItem;
+            string profesja = listBox1.SelectedItem.ToString();
             string lvl = textBox1.Text;
             string mlvl = textBox2.Text;
-            
-
-            string profesjaString = (profesja == null)
-                ? string.Empty
-                : profesja.ToString();
+            string profesjaString = PobierzProfesje(profesja);
 
             int poziomLiczba = 0;
             bool toJestLiczba = int.TryParse(lvl, out poziomLiczba);
@@ -46,7 +42,7 @@ namespace tibicalc
                 return;
             }
 
-            if(toJestLiczba)
+            if (toJestLiczba)
             {
                 if (poziomLiczba < 0)
                 {
@@ -89,44 +85,128 @@ namespace tibicalc
                 return;
             }
 
-            int poziomOpuszczeniaRookgaard = 8;
-            int punktyZycia = 0;
-            int doswiadczenie = ((50 * (int)(Math.Pow((poziomLiczba - 1), 3))) - (150 * (int)(Math.Pow((poziomLiczba - 1), 2))) + (400 * (poziomLiczba -1)))/3;
+            int poziomOpuszczeniaRookgaard = PoziomWyjsciaZRooka();
+            int doswiadczenie = WyliczDoswiadczenie(poziomLiczba);
+            int expdolvla = WyliczExpDoLvl(poziomLiczba);
+            int baseSpeed = WyliczBaseSpeed(poziomLiczba);
+            int PunktyZycia = WyliczPunktyZycia(profesjaString, poziomLiczba, poziomOpuszczeniaRookgaard);
+            int punktyMany = WyliczPunktyMany(profesjaString, poziomLiczba, poziomOpuszczeniaRookgaard);
+            int capacity = WyliczCapacity(profesjaString, poziomLiczba, poziomOpuszczeniaRookgaard);
+            decimal manaNaMlvl = WyliczManeNaMLvl(profesjaString, mlvlLiczba);
+            
+
+            richTextBox1.Text = string.Format("Profesja: {0}\nLVL: {1}\nMLVL: {2}\n\nDoświadczenie: {3} exp\nDo LVL'a: {4} exp\nDo MLVL'a: {5}\n\nCapacity: {6}\nPunkty zycia: {7}\nPunkty many: {8}\nSpeed: {9} ", profesja, lvl, mlvl, doswiadczenie, expdolvla, manaNaMlvl, capacity, PunktyZycia, punktyMany, baseSpeed);
+            //"Profesja: " + profesja + "\nPoziom: " + poziom;
+        }
+
+        private static int PoziomWyjsciaZRooka()
+        {
+            return 8;
+        }
+        private static string PobierzProfesje(object profesja)
+        {
+            return (profesja == null)
+                ? string.Empty
+                : profesja.ToString();
+        }
+        private int WyliczDoswiadczenie(int poziomLiczba)
+        {
+            int doswiadczenie = ((50 * (int)(Math.Pow((poziomLiczba - 1), 3))) - (150 * (int)(Math.Pow((poziomLiczba - 1), 2))) + (400 * (poziomLiczba - 1))) / 3;
+
+            return doswiadczenie;
+        }
+        private int WyliczExpDoLvl(int poziomLiczba)
+        {
             int expdolvla = 50 * (int)(Math.Pow(poziomLiczba, 2)) - (150 * poziomLiczba) + 200;
-            int punktyMany = 0;
-            int capacity = 0;
-            decimal manaNaMlvl = 0;
+
+            return expdolvla;
+        }
+        private int WyliczBaseSpeed(int poziomLiczba)
+        {
             int baseSpeed = 110 + poziomLiczba - 1;
+            return baseSpeed;
+        }
+        private int WyliczPunktyZycia(string profesjaString, int poziomLiczba, int poziomOpuszczeniaRookgaard)
+        {
+            int punktyZycia = 0;
 
             switch (profesjaString)
             {
                 case "Knight":
                     punktyZycia = 5 * ((3 * poziomLiczba) - 2 * poziomOpuszczeniaRookgaard + 29);
-                    punktyMany = 5 * (poziomLiczba + 10);
-                    capacity = 5 * ((5 * poziomLiczba) - (5 * poziomOpuszczeniaRookgaard) + 94);
-                    manaNaMlvl = 1600 * 3m * mlvlLiczba;
                     break;
                 case "Paladin":
-                    punktyZycia = 5 *((2 * poziomLiczba) - 8 + 29);
-                    punktyMany = 5 * ((3 * poziomLiczba) - (2 * poziomOpuszczeniaRookgaard) + 10);
-                    capacity = 10 * ((2 * poziomLiczba) - poziomOpuszczeniaRookgaard + 39);
-                    manaNaMlvl = (1600 * 1.4m * mlvlLiczba);
+                    punktyZycia = 5 * ((2 * poziomLiczba) - poziomOpuszczeniaRookgaard + 29);
                     break;
                 case "Sorc/Druid":
                     punktyZycia = 5 * (poziomLiczba + 29);
-                    punktyMany = 5 * ((6 * poziomLiczba) - (5 * poziomOpuszczeniaRookgaard) + 10);
-                    capacity = 10 * (poziomLiczba + 39);
-                    manaNaMlvl = 1600 * 1.1m * mlvlLiczba;
                     break;
                 default:
-                    punktyZycia = 5 *(poziomLiczba + 29);
+                    punktyZycia = 5 * (poziomLiczba + 29);
+                    break;
+                    // liczenie...
+                    // punktyZycia = xxx
+                   
+            }
+            return punktyZycia;
+        }
+        private int WyliczPunktyMany(string profesjaString, int poziomLiczba, int poziomOpuszczeniaRookgaard)
+        {
+            int punktyMany = 0;
+
+            switch (profesjaString)
+            {
+                case "Knight":
                     punktyMany = 5 * (poziomLiczba + 10);
+                    break;
+                case "Paladin":
+                    punktyMany = 5 * ((3 * poziomLiczba) - (2 * poziomOpuszczeniaRookgaard) + 10);
+                    break;
+                case "Sorc/Druid":
+                    punktyMany = 5 * ((6 * poziomLiczba) - (5 * poziomOpuszczeniaRookgaard) + 10);
+                    break;
+                default:
+                    punktyMany = 5 * (poziomLiczba + 10);
+                    break;
+            }
+            return punktyMany;
+        }
+        private int WyliczCapacity(string profesjaString, int poziomLiczba, int poziomOpuszczeniaRookgaard)
+        {
+            int capacity = 0;
+            switch (profesjaString)
+            {
+                case "Knight":
+                    capacity = 5 * ((5 * poziomLiczba) - (5 * poziomOpuszczeniaRookgaard) + 94);
+                    break;
+                case "Paladin":
+                    capacity = 10 * ((2 * poziomLiczba) - poziomOpuszczeniaRookgaard + 39);
+                    break;
+                case "Sorc/Druid":
+                    capacity = 10 * (poziomLiczba + 39);
+                    break;
+                default:
                     capacity = 10 * (poziomLiczba + 39);
                     break;
             }
-      
-            richTextBox1.Text = string.Format("Profesja: {0}\nLVL: {1}\nMLVL: {2}\n\nDoświadczenie: {3} exp\nDo LVL'a: {4} exp\nDo MLVL'a: {5}\n\nCapacity: {6}\nPunkty zycia: {7}\nPunkty many: {8}\nSpeed: {9} ", profesja, lvl, mlvl, doswiadczenie, expdolvla, manaNaMlvl , capacity, punktyZycia, punktyMany, baseSpeed  );
-            //"Profesja: " + profesja + "\nPoziom: " + poziom;
+            return capacity;
+        }
+        private decimal WyliczManeNaMLvl(string profesjaString, int mlvlLiczba)
+        {
+            decimal manaNaMlvl = 0;
+            switch (profesjaString)
+            {
+                case "Knight":
+                    manaNaMlvl = 1600 * 3m * mlvlLiczba;
+                    break;
+                case "Paladin":
+                    manaNaMlvl = (1600 * 1.4m * mlvlLiczba);
+                    break;
+                case "Sorc/Druid":
+                    manaNaMlvl = 1600 * 1.1m * mlvlLiczba;
+                    break;
+            }
+            return manaNaMlvl;
         }
     }
 }
